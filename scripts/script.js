9d336d7dd329425725f786268808fe3ad9c2f5d5
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2023-12-01 09:53:01 +0100

    nieuw home page

--- scripts/script.js
+++ scripts/script.js
@@ -60,51 +60,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let scrollAmount = 400; // het aantal pixels om te scrollen
 
-    scrollLeftButton.addEventListener("click", function () {
+    scrollLeftButton.addEventListener("click", 
+    function () {
         ulBlok.scrollLeft -= scrollAmount;
     });
 
-    scrollRightButton.addEventListener("click", function () {
+    scrollRightButton.addEventListener("click", 
+    function () {
         ulBlok.scrollLeft += scrollAmount;
     });
 });
 
-
-//     // For the second set of buttons
-//     const ulBlok2 = document.querySelector(".ul-blok2"); // Adjust the selector
-//     const scrollLeftButton2 = document.querySelector(".scroll-links2"); // Adjust the selector
-//     const scrollRightButton2 = document.querySelector(".scroll-rechts2"); // Adjust the selector
-
-//     scrollLeftButton2.addEventListener("click", function () {
-//         ulBlok2.scrollLeft -= scrollAmount;
-//     });
-
-//     scrollRightButton2.addEventListener("click", function () {
-//         ulBlok2.scrollLeft += scrollAmount;
-//     });
-// });
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     // Plaats hier de JavaScript-code
-//     const scrollButtonContainers = document.querySelectorAll(".scroll-buttons");
-
-//     scrollButtonContainers.forEach(function (container) {
-//         const ulBlok = container.querySelector(".ul-blok");
-//         const scrollLeftButton = container.querySelector(".scroll-links");
-//         const scrollRightButton = container.querySelector(".scroll-rechts");
-
-//         let scrollAmount = 300;
-
-//         scrollLeftButton.addEventListener("click", function () {
-//             ulBlok.scrollLeft -= scrollAmount;
-//         });
-
-//         scrollRightButton.addEventListener("click", function () {
-//             ulBlok.scrollLeft += scrollAmount;
-//         });
-//     });
-// });
 
 
 
